1506dee77c8be1d537de61f679e6218521e5eb63
DerrickYLJ <99985904+DerrickYLJ@users.noreply.github.com>
2022-07-19 00:48:18 -0400

    Update virtuallink_calculation_10.py

--- Virtual_Links_Simulation/virtuallink_calculation_10.py
+++ Virtual_Links_Simulation/virtuallink_calculation_10.py
@@ -1,36 +1,32 @@
 from pycpa import *
 
-# case 1 : VL1
+# case 15 : VL10
 # VL10 -> VL5 -> VL12 -> switch_A (VL1) to Switch B -> VL1 -> VL8 -> VL11 -> VL9 ->switch B (VL1) to ES4 
+# ES3 -> (VL5 + VL12) -> Switch A -> (VL1 + VL8 + VL11) -> Switch B -> (VL9) -> ES4
 
 # generate an new system
 s = model.System('compare')
 
 # set up all the resouces, or vertexes
-VL10 = s.bind_resource(model.Resource("FromSource", schedulers.SPNPScheduler()))
-VL5 = s.bind_resource(model.Resource("VL2", schedulers.SPNPScheduler()))
-VL12 = s.bind_resource(model.Resource("VL3", schedulers.SPNPScheduler()))
-switchA_to_B = s.bind_resource(model.Resource("switchB_to_A", schedulers.SPNPScheduler()))
-VL1 = s.bind_resource(model.Resource("VL9", schedulers.SPNPScheduler()))
-VL8 = s.bind_resource(model.Resource("VL5", schedulers.SPNPScheduler()))
-VL11 = s.bind_resource(model.Resource("VL9", schedulers.SPNPScheduler()))
-VL9 = s.bind_resource(model.Resource("VL9", schedulers.SPNPScheduler()))
-switchB_to_ES4 = s.bind_resource(model.Resource("switchA_to_ES1", schedulers.SPNPScheduler()))
+source_fromES3 = s.bind_resource(model.Resource("source_fromES3", schedulers.SPNPScheduler()))
+first = s.bind_resource(model.Resource("first", schedulers.SPNPScheduler()))
+switchA_to_B = s.bind_resource(model.Resource("switchA_to_B", schedulers.SPNPScheduler()))
+second = s.bind_resource(model.Resource("second", schedulers.SPNPScheduler()))
+switchB_to_ES4 = s.bind_resource(model.Resource("switchB_to_ES4", schedulers.SPNPScheduler()))
+third = s.bind_resource(model.Resource("third", schedulers.SPNPScheduler()))
 
 #create tasks related to the resources
-t1 = VL10.bind_task(model.Task("T1", wcet=30, bcet=1, scheduling_parameter=2))
-t2 = VL5.bind_task(model.Task("T2", wcet=30, bcet=1, scheduling_parameter=2))
-t3 = VL12.bind_task(model.Task("T3", wcet=14, bcet=1, scheduling_parameter=2))
-t4 = switchA_to_B.bind_task(model.Task("T4", wcet=30, bcet=1, scheduling_parameter=2))
-t5 = VL1.bind_task(model.Task("T3", wcet=8, bcet=1, scheduling_parameter=2))
-t6 = VL8.bind_task(model.Task("T3", wcet=14, bcet=1, scheduling_parameter=2))
-t7 = VL11.bind_task(model.Task("T3", wcet=121, bcet=1, scheduling_parameter=2))
-t8 = VL9.bind_task(model.Task("T3", wcet=121, bcet=1, scheduling_parameter=2))
-t9 = switchB_to_ES4.bind_task(model.Task("T3", wcet=30, bcet=1, scheduling_parameter=2))
+t1 = source_fromES3.bind_task(model.Task("T1", wcet=30, bcet=1, scheduling_parameter=2))
+t2 = first.bind_task(model.Task("VL5 + VL12", wcet=22, bcet=1, scheduling_parameter=2))
+t3 = switchA_to_B.bind_task(model.Task("T3", wcet=30, bcet=1, scheduling_parameter=2))
+t4 = second.bind_task(model.Task("VL1 + VL8 + VL11", wcet=143, bcet=1, scheduling_parameter=2))
+t5 = switchB_to_ES4.bind_task(model.Task("T5", wcet=30, bcet=1, scheduling_parameter=2))
+t6 = third.bind_task(model.Task("VL9", wcet=121, bcet=1, scheduling_parameter=2))
 
-t1.link_dependent_task(t2).link_dependent_task(t3).link_dependent_task(t4).link_dependent_task(t5).link_dependent_task(t6).link_dependent_task(t7).link_dependent_task(t8).link_dependent_task(t9)
+# specify precedence constraints and link all the tasks altogether:
+t1.link_dependent_task(t2).link_dependent_task(t3).link_dependent_task(t4).link_dependent_task(t5).link_dependent_task(t6)
 
-t1.in_event_model = model.PJdEventModel(P=64000, J=0)
+t1.in_event_model = model.PJdEventModel(P=8000, J=0)
 
 print("\nPerforming analysis of system '%s'" % s.name)
 task_results = analysis.analyze_system(s)
@@ -43,7 +39,7 @@ for r in sorted(s.resources, key=str):
         print("    b_wcrt=%s" % (task_results[t].b_wcrt_str()))
 
 # specify paths
-p1 = s.bind_path(model.Path("P1", [t1, t2, t3, t4, t5, t6, t7, t8, t9]))
+p1 = s.bind_path(model.Path("P1", [t1, t2, t3, t4, t5, t6]))
 
 paths = [p1]
 # perform path analysis
